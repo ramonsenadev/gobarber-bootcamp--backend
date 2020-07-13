eb3646671f05d1a9a163ebd74b17e256324a464c
Ramon Sena <ramonsenadev@gmail.com>
2020-07-13 18:12:40 -0300

    Add cors middleware

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -13,6 +14,7 @@ const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use(cors());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
